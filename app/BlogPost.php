edf7d05fad9541a5eedccfc930b34cc196c014dc
Lakhan Bisht <lakhanbisht9@gmail.com>
2020-08-08 17:28:31 +0530

    83-Authorization Introduction

--- app/BlogPost.php
+++ app/BlogPost.php
@@ -3,10 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BlogPost extends Model
 {
     // protected $table = 'blogposts';
+
+    use SoftDeletes;
 
     protected $fillable = ['title', 'content'];
 
@@ -15,12 +18,21 @@ class BlogPost extends Model
         return $this->hasMany('App\Comment');
     }
 
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+    
     public static function boot()
     {
         parent::boot();
 
-        // static::deleting(function (BlogPost $blogPost) {
-        //     $blogPost->comments()->delete();
-        // });
+        static::deleting(function (BlogPost $blogPost) {
+            $blogPost->comments()->delete();
+        });
+
+        static::restoring(function (BlogPost $blogPost) {
+            $blogPost->comments()->restore();
+        });
     }
 }
